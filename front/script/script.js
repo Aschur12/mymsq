b2006ccf08a9c2919071a70063e09d57e6572885
Andrey Schur <aschur1979@gmail.com>
2022-02-24 21:19:42 +0200

    New_24_02_2022

---
+++ front/script/script.js
@@ -0,0 +1,17 @@
+function kurses_usd () {
+	let kurs_usd;
+	const xhr = new XMLHttpRequest();
+	const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+	xhr.open('get', url, false);
+	xhr.send();
+	let kurs_usd_arr = JSON.parse(xhr.responseText);
+	kurs_usd_arr.map((kurs) => {
+		if (kurs['r030'] == '840') {
+			kurs_usd = kurs['rate'];
+		}
+	})
+	return kurs_usd;
+};
+let k_usd = kurses_usd();
+console.log(k_usd);
+
